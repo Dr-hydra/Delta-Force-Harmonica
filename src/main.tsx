f7116b17d07cfe3f98a52872d7ce76bd6d2f49d5
Dr-hydra <116339679+Dr-hydra@users.noreply.github.com>
2026-09-11 17:33:23 +0800

    feat: expose converter cloud actions in main UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import ConverterCloudPanel from "./cloud/ConverterCloudPanel";
 import CloudLibraryPage from "./library/CloudLibraryPage";
 import "./styles.css";
 
@@ -12,6 +13,7 @@ function Root() {
   return (
     <>
       <App />
+      <ConverterCloudPanel />
       <a className="cloud-library-launcher" href={`${window.location.pathname}?view=library`}>
         CLOUD LIBRARY
         <span>公开曲谱 · Toy 云存档</span>
